a7352dab8915ed494ce8e8c88382e40d0599811a
yigzawchalie00-tech <yigzawchalie00@gmail.com>
2026-08-19 08:27:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -603,4 +603,12 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import sys
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.close()
